f5fe2c8d15fa2b078336390824953380366a9814
ttre16 <ilia.tretiak.2001@mail.ru>
2019-12-27 00:09:13 +0300

    initial commit

---
+++ src/main/java/SecurityServlet.java
@@ -0,0 +1,13 @@
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+@WebServlet("/security")
+public class SecurityServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
