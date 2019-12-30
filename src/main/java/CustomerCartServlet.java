9f1f6755e724a2a1e6ebc097623f6a2efdc3286a
ttre16 <ilia.tretiak.2001@mail.ru>
2019-12-30 16:16:14 +0300

    added 1

---
+++ src/main/java/CustomerCartServlet.java
@@ -0,0 +1,46 @@
+import logic.Cart;
+import logic.Customer;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+
+@WebServlet("jordan")
+public class CustomerCartServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        HttpSession session = request.getSession();
+        response.setContentType("text/html");
+        Customer customer = (Customer) session.getAttribute("customer");
+        if(customer!=null && customer.isLogin()) {
+
+            if (request.getParameter("name").equals("removeAll"))
+                customer.getCart().removeAll();
+            else {
+                if ((Boolean) session.getAttribute("add")) {
+                    customer.getCart().addProduct(request.getParameter("name"));
+                } else {
+                    customer.getCart().removeProduct(request.getParameter("name"));
+                }
+            }
+        }
+        response.sendRedirect("/head/cart.jsp");
+    }
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        Customer customer = (Customer) session.getAttribute("customer");
+        Cart cart = new Cart();
+        customer.setCart(cart);
+        cart.setCustomerName(customer.getName());
+        cart.load();
+        response.sendRedirect("/head/cart.jsp");
+    }
+
+}
